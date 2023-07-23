0f7a4c685b64abf8648df5ccff51c27059001b55
user.email <jobayer.ahmed109@gmail.com>
2023-07-23 22:09:02 +0600

    Initial server setup done

---
+++ index.js
@@ -0,0 +1,20 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Hello from server");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http//:localhost:${port}`);
+});
